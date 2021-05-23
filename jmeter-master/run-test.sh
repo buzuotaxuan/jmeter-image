0950f1cd5b2e95484894201c0cc3e6e892e21375
Captain.B <bin@fit2cloud.com>
2021-05-23 22:51:39 +0800

    refactor: 执行前进行节点同步

--- jmeter-master/run-test.sh
+++ jmeter-master/run-test.sh
@@ -1,5 +1,11 @@
 wget --no-check-certificate "${METERSPHERE_URL}/jmeter/download?testId=${TEST_ID}&resourceId=${RESOURCE_ID}&ratio=${RATIO}&reportId=${REPORT_ID}&resourceIndex=${RESOURCE_INDEX}" -O ${TEST_ID}.zip
-unzip -o ${TEST_ID}.zip -d ${TESTS_DIR} 
+unzip -o ${TEST_ID}.zip -d ${TESTS_DIR}
+
+while [[ $(curl -s ${METERSPHERE_URL}/jmeter/ready?&ratio=${RATIO}&resourceIndex=${RESOURCE_INDEX}&reportId=${REPORT_ID}) -gt  0 ]]
+do
+  sleep 0.1
+done
+
 for file in ${TESTS_DIR}/*.jmx; do
   jmeter -n -t ${file} -Jserver.rmi.ssl.disable=${SSL_DISABLED}
 done

7c47616081835f602b85ed1686afe306f73dfb80
Captain.B <bin@fit2cloud.com>
2021-07-22 15:01:13 +0800

    refactor: 修改镜像，支持 dns config

--- jmeter-master/run-test.sh
+++ jmeter-master/run-test.sh
@@ -1,11 +1,6 @@
 wget --no-check-certificate "${METERSPHERE_URL}/jmeter/download?testId=${TEST_ID}&resourceId=${RESOURCE_ID}&ratio=${RATIO}&reportId=${REPORT_ID}&resourceIndex=${RESOURCE_INDEX}" -O ${TEST_ID}.zip
 unzip -o ${TEST_ID}.zip -d ${TESTS_DIR}
 
-while [[ $(curl -s -G -d "ratio=${RATIO}" -d "resourceIndex=${RESOURCE_INDEX}" -d "reportId=${REPORT_ID}" ${METERSPHERE_URL}/jmeter/ready) -gt  0 ]]
-do
-  sleep 0.2
-done
-
 # check file
 if [ ! -f "${TESTS_DIR}/ms.properties" ]; then
   touch "${TESTS_DIR}/ms.properties"
@@ -15,6 +10,11 @@ fi
 if [ ! -f "${TESTS_DIR}/hosts" ]; then
   cat ${TESTS_DIR}/hosts >> /etc/hosts
 fi
+
+while [[ $(curl -s -G -d "ratio=${RATIO}" -d "resourceIndex=${RESOURCE_INDEX}" -d "reportId=${REPORT_ID}" ${METERSPHERE_URL}/jmeter/ready) -gt  0 ]]
+do
+  sleep 0.2
+done
 
 for file in ${TESTS_DIR}/*.jmx; do
   jmeter -n -t ${file} -Jserver.rmi.ssl.disable=${SSL_DISABLED} -p ${TESTS_DIR}/ms.properties

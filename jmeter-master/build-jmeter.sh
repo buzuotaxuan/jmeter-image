6ca1ef87636ab7f0541d1e9084679f5623cde70d
CaptainB <bin@fit2cloud.com>
2021-11-02 13:34:39 +0800

    refactor: dockerfile

--- jmeter-master/build-jmeter.sh
+++ jmeter-master/build-jmeter.sh
@@ -4,12 +4,12 @@ unzip -o jpgc-casutg-2.9.zip -d /tmp/ && mv /tmp/lib/ext/jmeter-plugins-casutg-2
 unzip -o jpgc-tst-2.5.zip -d /tmp/ && mv /tmp/lib/ext/jmeter-plugins-tst-2.5.jar ${JMETER_HOME}/lib/ext && \
 mv metersphere-jmeter-functions-v1.0.1.jar ${JMETER_HOME}/lib/ext && \
 mv jmeter-plugins-dubbo-${DUBBO_JMETER_PLUGIN_VERSION}-jar-with-dependencies.jar ${JMETER_HOME}/lib/ext && \
-mv mysql-connector-java.jar ${JMETER_HOME}/lib/ext && \
 mv jmeter.backendlistener.kafka-${KAFKA_BACKEND_LISTENER_VERSION}.jar ${JMETER_HOME}/lib/ext && \
-mv ojdbc8.jar ${JMETER_HOME}/lib/ext && \
-mv postgresql.jar ${JMETER_HOME}/lib/ext && \
-mv mssql-jdbc.jar ${JMETER_HOME}/lib/ext && \
-mv jmeter-plugins-threadgroup-autostop.jar ${JMETER_HOME}/lib/ext && \
+mv mysql-connector-java-5.1.49.jar ${JMETER_HOME}/lib/ext && \
+mv ojdbc8-19.7.0.0.jar ${JMETER_HOME}/lib/ext && \
+mv postgresql-42.2.14.jar ${JMETER_HOME}/lib/ext && \
+mv mssql-jdbc-7.4.1.jre8.jar ${JMETER_HOME}/lib/ext && \
+mv jmeter-plugins-threadgroup-autostop-0.1.jar ${JMETER_HOME}/lib/ext && \
 mv jython-standalone-2.7.2.jar ${JMETER_HOME}/lib/ext && \
 mv generate-report-0.3.1.jar /jmeter-log/generate-report.jar && \
 ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo "$TZ" > /etc/timezone && \
--- jmeter-master/download.sh
+++ jmeter-master/download.sh
@@ -17,8 +17,8 @@ JMETER_VERSION="5.4.1" &&
   wget -N --no-check-certificate https://hub.fastgit.org/metersphere/jmeter-plugins-for-apache-dubbo/releases/download/${DUBBO_JMETER_PLUGIN_VERSION}/jmeter-plugins-dubbo-${DUBBO_JMETER_PLUGIN_VERSION}-jar-with-dependencies.jar &&
   wget -N --no-check-certificate https://hub.fastgit.org/metersphere/generate-report-release/releases/download/v0.3.1/generate-report-0.3.1.jar &&
   wget -N --no-check-certificate https://repo1.maven.org/maven2/org/python/jython-standalone/2.7.2/jython-standalone-2.7.2.jar &&
-  wget -N http://search.maven.org/remotecontent?filepath=mysql/mysql-connector-java/5.1.49/mysql-connector-java-5.1.49.jar -O mysql-connector-java.jar &&
-  wget -N http://search.maven.org/remotecontent?filepath=com/oracle/database/jdbc/ojdbc8/19.7.0.0/ojdbc8-19.7.0.0.jar -O ojdbc8.jar &&
-  wget -N http://search.maven.org/remotecontent?filepath=org/postgresql/postgresql/42.2.14/postgresql-42.2.14.jar -O postgresql.jar &&
-  wget -N http://search.maven.org/remotecontent?filepath=com/microsoft/sqlserver/mssql-jdbc/7.4.1.jre8/mssql-jdbc-7.4.1.jre8.jar -O mssql-jdbc.jar &&
-  wget -N http://search.maven.org/remotecontent?filepath=io/metersphere/jmeter-plugins-threadgroup-autostop/0.1/jmeter-plugins-threadgroup-autostop-0.1.jar -O jmeter-plugins-threadgroup-autostop.jar
+  wget -N --no-check-certificate https://repo1.maven.org/maven2/mysql/mysql-connector-java/5.1.49/mysql-connector-java-5.1.49.jar &&
+  wget -N --no-check-certificate https://repo1.maven.org/maven2/com/oracle/database/jdbc/ojdbc8/19.7.0.0/ojdbc8-19.7.0.0.jar &&
+  wget -N --no-check-certificate https://repo1.maven.org/maven2/org/postgresql/postgresql/42.2.14/postgresql-42.2.14.jar &&
+  wget -N --no-check-certificate https://repo1.maven.org/maven2/com/microsoft/sqlserver/mssql-jdbc/7.4.1.jre8/mssql-jdbc-7.4.1.jre8.jar &&
+  wget -N --no-check-certificate https://repo1.maven.org/maven2/io/metersphere/jmeter-plugins-threadgroup-autostop/0.1/jmeter-plugins-threadgroup-autostop-0.1.jar

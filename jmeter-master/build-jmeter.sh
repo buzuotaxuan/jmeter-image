f5829c0f2943a84f7677b9880008a685f6d7a7eb
CaptainB <bin@fit2cloud.com>
2021-11-02 15:39:48 +0800

    refactor: 插件升级

--- jmeter-master/build-jmeter.sh
+++ jmeter-master/build-jmeter.sh
@@ -8,6 +8,8 @@ JMETER_VERSION="5.4.1" &&
   JMETER_HOME="${SCRIPT_DIR}/.cache/jmeter" &&
   mkdir -p "${SCRIPT_DIR}/.cache" &&
   cd "${SCRIPT_DIR}/.cache" &&
+  rm -rf ./jmeter &&
+  rm -rf ./lib &&
   wget -N --no-check-certificate https://mirrors.tuna.tsinghua.edu.cn/apache/jmeter/binaries/apache-jmeter-${JMETER_VERSION}.tgz &&
   wget -N --no-check-certificate https://jmeter-plugins.org/files/packages/jpgc-casutg-2.10.zip &&
   wget -N --no-check-certificate https://jmeter-plugins.org/files/packages/jpgc-tst-2.5.zip &&
@@ -23,14 +25,5 @@ JMETER_VERSION="5.4.1" &&
   wget -N --no-check-certificate https://repo1.maven.org/maven2/io/metersphere/jmeter-plugins-threadgroup-autostop/0.1/jmeter-plugins-threadgroup-autostop-0.1.jar &&
   mkdir -p ${JMETER_HOME} &&
   tar -zxf apache-jmeter-${JMETER_VERSION}.tgz -C ${JMETER_HOME}/ --strip-components=1 &&
-  unzip -o jpgc-casutg-2.10.zip -d ./ && cp ./lib/ext/jmeter-plugins-casutg-2.10.jar ${JMETER_HOME}/lib/ext &&
-  unzip -o jpgc-tst-2.5.zip -d ./ && cp ./lib/ext/jmeter-plugins-tst-2.5.jar ${JMETER_HOME}/lib/ext &&
-  cp metersphere-jmeter-functions-v1.0.1.jar ${JMETER_HOME}/lib/ext &&
-  cp jmeter-plugins-dubbo-${DUBBO_JMETER_PLUGIN_VERSION}-jar-with-dependencies.jar ${JMETER_HOME}/lib/ext &&
-  cp jmeter.backendlistener.kafka-${KAFKA_BACKEND_LISTENER_VERSION}.jar ${JMETER_HOME}/lib/ext &&
-  cp mysql-connector-java-5.1.49.jar ${JMETER_HOME}/lib/ext &&
-  cp ojdbc8-19.7.0.0.jar ${JMETER_HOME}/lib/ext &&
-  cp postgresql-42.2.14.jar ${JMETER_HOME}/lib/ext &&
-  cp mssql-jdbc-7.4.1.jre8.jar ${JMETER_HOME}/lib/ext &&
-  cp jmeter-plugins-threadgroup-autostop-0.1.jar ${JMETER_HOME}/lib/ext &&
-  cp jython-standalone-2.7.2.jar ${JMETER_HOME}/lib/ext
+  unzip -o jpgc-casutg-2.10.zip -d ./ &&
+  unzip -o jpgc-tst-2.5.zip -d ./
